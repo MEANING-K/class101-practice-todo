d1341ca4c5624956bf38d4953f1494e5614ca49d
MEANING-K <blossom9704@gmail.com>
2024-04-05 18:04:11 +0900

    feature: page layout, todo box base - Change NotFound Page location

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,14 +1,67 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+
+// 페이지 속 요소를 가운데로 해줌
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #eee;
+`;
+
+// 박스 컴포넌트
+const Box = styled.div`
+  width: 400px;
+  height: 600px;
+  background-color: white;
+  box-shadow: 0px 25px 100px -60px rgba(0, 0, 0, 0.18);
+`;
+
+// 타이틀 컴포넌트
+const Title = styled.h1`
+  margin: 0;
+  padding: 15px 25px;
+`;
+
+// 투두리스트 컴포넌트
+const TodoList = styled.div``;
+
+// 투두아이템 컴포넌트
+const TodoItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+  width: 100%;
+  font-size: 1.2em;
+  border-bottom: 1px solid #eee;
+`;
+
+// 투두체크 컴포넌트
+const TodoCheck = styled.input`
+  margin-right: 15px;
+`;
 
 export function HomePage() {
   return (
     <>
       <Helmet>
-        <title>HomePage</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>Main</title>
+        <meta name="description" content="Todo Main App" />
       </Helmet>
-      <span>My HomePage</span>
+      <Wrapper>
+        <Box>
+          <Title>할 일</Title>
+          <TodoList>
+            <TodoItem>
+              <TodoCheck type="checkbox" />
+              나는 투두
+            </TodoItem>
+          </TodoList>
+        </Box>
+      </Wrapper>
     </>
   );
 }
